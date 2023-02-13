d987fdc3cdf7497a5a32581e40c828c4a47fcb02
João Marcos <joaomarcosjmos.ans@gmail.com>
2023-02-13 23:13:42 +0000

    CRUD completo para cliente

---
+++ src/app/components/cliente/cliente-list/clientes-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from 'src/app/models/cliente';
+import {MatSort, Sort} from '@angular/material/sort';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+@Component({
+  selector: 'app-cliente-list',
+  templateUrl: './cliente-list.component.html',
+  styleUrls: ['./cliente-list.component.css']
+})
+export class ClienteListComponent implements OnInit {
+
+  ELEMENT_DATA: Cliente[] = []
+  
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private service: ClienteService,
+    private _liveAnnouncer: LiveAnnouncer) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA) //or resposta
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+}
