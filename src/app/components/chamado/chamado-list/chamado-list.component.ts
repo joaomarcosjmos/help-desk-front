1edfb5dc0864b1cb1f2876dd21d0b5c85f4582b2
João Marcos <joaomarcosjmos.ans@gmail.com>
2023-02-15 15:40:02 +0000

    HTML e CSS para componente chamado-list

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/chamados';
 
 @Component({
   selector: 'app-chamado-list',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Chamado[] = [{
+    id: 'any',
+    dataAbertura: 'string;',
+    dataFechamento: 'string;',
+    prioridade: 'string;',
+    status: 'string;',
+    titulo: 'string;',
+    descricao: 'string;',
+    tecnico: 1,
+    cliente: 6,
+    nomeCliente: 'joao;',
+    nomeTecnico:' marcos;',
+  }]
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private _liveAnnouncer: LiveAnnouncer
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
